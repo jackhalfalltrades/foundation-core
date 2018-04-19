a543ac737c2716854ad3123731bcdfd24ebd66f8
chandrakanthperavelli <chandrakanthperavelli@gmail.com>
2018-04-19 02:10:23 -0500

    enabled jar

--- src/main/java/com/jackhalfalltrades/foundation/datasource/FoundationDataSource.java
+++ src/main/java/com/jackhalfalltrades/foundation/datasource/FoundationDataSource.java
@@ -1,28 +1,49 @@
 package com.jackhalfalltrades.foundation.datasource;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.jackhalfalltrades.foundation.foundationConfig.PropertyManager;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import javax.sql.DataSource;
+@Configuration
+@ConfigurationProperties
+public class FoundationDataSource {
 
-    @Data
-    @ConfigurationProperties(prefix = "spring.datasource")
-    @Configuration
-    public class FoundationDataSource {
+    private String driverClassName;
 
-        private NamedParameterJdbcTemplate template;
+    private String url;
 
-        public FoundationDataSource() {
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSourceBuilder.create().build());
-            this.template = new NamedParameterJdbcTemplate(jdbcTemplate);
-        }
+    private String userName;
 
+    private String password;
+
+    public FoundationDataSource() {
+        this.driverClassName = PropertyManager.getProperty("spring.datasource.driver-class-name");
+        this.url = PropertyManager.getProperty("spring.datasource.url");
+        this.userName = PropertyManager.getProperty("spring.datasource.username");
+        this.password = PropertyManager.getProperty("spring.datasource.password");
     }
+
+    public FoundationDataSource(String driverClassName, String url, String userName, String password) {
+        this.driverClassName = driverClassName;
+        this.url = url;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    public NamedParameterJdbcTemplate getJdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(
+                DataSourceBuilder.create()
+                        .driverClassName(driverClassName)
+                        .url(url)
+                        .username(userName)
+                        .password(password)
+                        .build());
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
+
+
+}
